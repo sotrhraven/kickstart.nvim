eee98c7be8dee558e1a69a2e6f3de26b48d1ddc1
sotrhraven <sotrhravencode@gmail.com>
2024-01-09 16:51:47 -0500

    added clangd, bash-lsp, shmf

--- lua/configs/formatter.lua
+++ lua/configs/formatter.lua
@@ -1,29 +1,28 @@
 -- Utilities for creating configurations
 local util = require 'formatter.util'
 
-             -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-             require('formatter').setup {
-                     -- Enable or disable logging
-                     logging = true,
-                     -- Set the log level
-                     log_level = vim.log.levels.WARN,
-                     -- All formatter configurations are opt-in
-    filetype = {
-    lua = {
-        require('formatter.filetypes.lua').stylua,
-    },
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require('formatter').setup {
+  -- Enable or disable logging
+  logging = true,
+  -- Set the log level
+  log_level = vim.log.levels.WARN,
+  -- All formatter configurations are opt-in
+  filetype = {
+    lua = { require('formatter.filetypes.lua').stylua },
     c = { require('formatter.filetypes.c').astyle },
+    bash = { require('formatter.filetypes.sh').shfmt },
 
     ['*'] = {
-        require('formatter.filetypes.any').remove_trailing_whitespace,
+      require('formatter.filetypes.any').remove_trailing_whitespace,
     },
-},
-             }
+  },
+}
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-                augroup('__formatter__', { clear = true })
+augroup('__formatter__', { clear = true })
 autocmd('BufWritePost', {
-    group = '__formatter__',
-    command = 'FormatWrite',
+  group = '__formatter__',
+  command = 'FormatWrite',
 })
